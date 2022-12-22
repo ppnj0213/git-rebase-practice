2cb69a0ab3b24d56125a701202ac843380766766
ppnj0213 <ppnj0213@gmail.com>
2022-12-22 14:21:38 +0900

    Modify : login, main router 경로 수정

--- Frontend/git-rebase-test/src/Router.js
+++ Frontend/git-rebase-test/src/Router.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login/Login";
-import Main from "./pages/Main/Main";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
 
 export default function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/login' element={<Login />} />
+                <Route path='/main' element={<Main />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
